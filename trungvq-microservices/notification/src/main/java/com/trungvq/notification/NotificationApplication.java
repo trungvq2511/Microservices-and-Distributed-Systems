89d8326d17a4a09282349695c97ec37e011becff
cowboysaker2511 <cowboysaker2511@gmail.com>
2024-06-02 13:59:04 +0700

    init rabbitMQ, sample publish message

--- trungvq-microservices/notification/src/main/java/com/trungvq/notification/NotificationApplication.java
+++ trungvq-microservices/notification/src/main/java/com/trungvq/notification/NotificationApplication.java
@@ -1,13 +1,37 @@
 package com.trungvq.notification;
 
+import com.trungvq.messagequeue.producer.RabbitMQMessageProducer;
+import com.trungvq.notification.config.NotificationConfig;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.trungvq.notification",
+                "com.trungvq.messagequeue"}
+)
 @EnableEurekaClient
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
     }
+
+//    // test message queue
+//    @Bean
+//    CommandLineRunner commandLineRunner(
+//            RabbitMQMessageProducer producer,
+//            NotificationConfig notificationConfig) {
+//        return args -> {
+//            producer.publish(
+//                    new Person("Vu", "Trung", 25),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String firstName, String lastName, int age) {
+//    }
 }
